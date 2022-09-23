6caffb7b372154e5a2cd446cb1ab69b7ab758d2d
Maksim <maksimmaksim.setic.dev@gmail.com>
2022-09-23 15:28:33 +0200

    Seventh commit, conditionally renderred appropriate vectors, finishing up

--- src/Components/Card/Card.js
+++ src/Components/Card/Card.js
@@ -3,56 +3,36 @@ import "./Card.css";
 import "../CardPage/CardPage.css";
 import Axios from "axios";
 import styled from "styled-components";
-import { ReactComponent as Vector2 } from "../../Assets/Vector2.svg";
-import { ReactComponent as Vector3 } from "../../Assets/Vector3.svg";
+import { ReactComponent as Vector22 } from "../../Assets/Vector2.svg";
+import { ReactComponent as Vector33 } from "../../Assets/Vector3.svg";
 import { ReactComponent as Vector4 } from "../../Assets/Vector4.svg";
 import { ReactComponent as Vector5 } from "../../Assets/Vector5.svg";
 
 const CardGrid = styled.div`
-  display: flex;
-  margin-top: 116px;
-  @media (max-width: 760px) {
-    flex-direction: column;
-    display: inline-block;
-  }
-  .parent {
-    display: grid;
-    grid-template-columns: repeat(4, 1fr);
-    grid-template-rows: repeat(4, 1fr);
-    grid-column-gap: 13px;
-    grid-row-gap: 13px;
-  }
-  .div1 {
-    grid-area: 1 / 1 / 6 / 2;
-  }
-  .div2 {
-    grid-area: 1 / 2 / 6 / 3;
-  }
-  .div3 {
-    grid-area: 1 / 3 / 6 / 4;
-  }
-  .div4 {
-    grid-area: 1 / 4 / 6 / 5;
-  }
+display: flex;
+margin-top: 116px;
+.parent {
+  display: grid;
+  grid-template-columns: repeat(4, 1fr);
+  grid-template-rows: repeat(4, 1fr);
+  grid-column-gap: 13px;
+  grid-row-gap: 13px;
+ }
+
+section:first-child{
+  background: yellow;
+}
+@media (max-width: 760px) {
+  flex-direction: column;
+  display: inline-block;
+}
 `;
+
+const Vector = { img: <Vector22 />}
+const Vector2 = { img: <Vector33 />}
 
 const Card = () => {
   const [cardData, setCardData] = useState([]);
-
-  const vectorArray = [
-    {
-      img: <Vector2 />,
-    },
-    {
-      img2: <Vector3 />,
-    },
-    {
-      img3: <Vector4 />,
-    },
-    {
-      img4: <Vector5 />,
-    },
-  ];
 
   useEffect(() => {
     Axios.get("https://api.infosit-zadatak.tk/api/entity")
@@ -61,18 +41,31 @@ const Card = () => {
   }, []);
   console.log(cardData);
 
-  const cardRender = cardData.slice(0, 4).map((card, index) => {
+
+ 
+
+
+  const cardRender = cardData.slice(0,4).map((card, index) => {
+
+
+    const VectorPass = () => { 
+    if(index === 0 || index === 2) 
+    {return( <section className="cardshape">{Vector.img}</section>)}
+    
+    if(index === 1 || index === 3) 
+    {return( <section className="cardshape">{Vector2.img}</section>)}
+    }
+
+
     return (
-      <>
+      <> 
         <div className="cardbox" id="cardbox">
           <div className="cardbox2" id="cardbox2">
             <div className="cardbox3" id="cardbox3">
-              <div className="cardbox3" id="cardbox4">
+              <div className="cardbox4" id="cardbox4">
                 <img src={card.avatar} className="cardimg" />
 
-                {vectorArray.slice(0, 1).map((vector, index) => (
-                  <div className="cardshape">{vector.img}</div>
-                ))}
+           <VectorPass />
 
                 <div className="cardinfo">
                   <div className="metainfo">
@@ -95,10 +88,11 @@ const Card = () => {
             </div>
           </div>
         </div>
+ 
       </>
     );
-  });
-
+});
+  console.log(cardRender);
   return (
     <>
       <div>
@@ -107,5 +101,7 @@ const Card = () => {
     </>
   );
 };
+
+
 
 export default Card;
